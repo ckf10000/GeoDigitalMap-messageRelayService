2ff6acc1e72b324b73705c5898374b4604c2cc94
zhouhanlin <ckf10000@sina.com>
2025-03-15 17:42:57 +0800

    a

--- internal/controller/maintenance/maintenance_v1_service_health_check.go
+++ internal/controller/maintenance/maintenance_v1_service_health_check.go
@@ -7,6 +7,6 @@ import (
 
 func (c *ControllerV1) ServiceHealthCheck(ctx context.Context, req *v1.ServiceHealthCheckReq) (res *v1.ServiceHealthCheckRes, err error) {
 	str := "The current service status is very healthy"
-	//g.Log().Info(ctx, str)  // 健康检查的时候，频率太高，容易打印很多日志，此处不建议打印日志
+	//g.Log(consts.RestAPIService).Info(ctx, str)  // 健康检查的时候，频率太高，容易打印很多日志，此处不建议打印日志
 	return &v1.ServiceHealthCheckRes{Description: str}, err
 }
